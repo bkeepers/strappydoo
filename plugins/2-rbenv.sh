84dfe3c8303933cea12626ce8112f3ddc6841993
Brandon Keepers <bkeepers@github.com>
2018-03-11 11:07:28 -0500

    Fix rbenv after 80129a757bc9bf7fac6d5c8ebaea2a3226f47889

--- plugins/2-rbenv.sh
+++ plugins/2-rbenv.sh
@@ -2,7 +2,7 @@
 
 ( test -f ".ruby-version" && test -z "$(rbenv version-name 2>/dev/null)" ) || return 1
 
-_rbenv_bootstrap() {
+rbenv_bootstrap() {
   run rbenv install --skip-existing
   which bundle >/dev/null 2>&1  || {
     run gem install bundler
